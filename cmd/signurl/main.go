93a8f18da23c9ef685685c6d6c6be298cb509889
Stephen Soltesz <stephen.soltesz@gmail.com>
2017-12-31 16:35:05 -0500

    Add new options and use signed url to put data in GCS

--- cmd/signurl/main.go
+++ cmd/signurl/main.go
@@ -6,13 +6,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"cloud.google.com/go/storage"
 )
 
 var (
-	keyfile = flag.String("key", "", "Path fo name of PRM file.")
+	keyfile  = flag.String("key", "", "Path fo name of JSON key file.")
+	datafile = flag.String("data", "", "Path fo name of data file.")
+	bucket   = flag.String("bucket", "soltesz-urlsign-mlab-sandbox",
+		"Name of GCS bucket (without gs:// prefix), the given key must "+
+			"have write privileges for this bucket.")
 )
 
 // Store key in datastore.
@@ -34,7 +40,7 @@ func main() {
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	url, err := storage.SignedURL("soltesz-urlsign-mlab-sandbox", "sample.txt", &storage.SignedURLOptions{
+	url, err := storage.SignedURL(*bucket, *datafile, &storage.SignedURLOptions{
 		GoogleAccessID: data["client_id"],
 		PrivateKey:     []byte(data["private_key"]),
 		Method:         "PUT",
@@ -44,4 +50,18 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(url)
+	c := &http.Client{}
+	in, err := os.Open(*datafile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest("PUT", url, in)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", resp)
 }
